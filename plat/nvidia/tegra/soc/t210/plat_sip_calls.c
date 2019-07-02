3e5992a66d3bea1dc77b0c6b7bc01ead33eb5e95
Billy Laws <blaws05@gmail.com>
2019-07-02 18:35:32 +0100

    Add support for storing emc table location

--- plat/nvidia/tegra/soc/t210/plat_sip_calls.c
+++ plat/nvidia/tegra/soc/t210/plat_sip_calls.c
@@ -28,9 +28,16 @@
 #define PMC_WRITE 			U(0xbb)
 
 /*******************************************************************************
+ * EMC parameters
+ ******************************************************************************/
+#define EMC_TABLE_ADDR		U(0xaa)
+#define EMC_TABLE_SIZE		U(0xbb)
+
+/*******************************************************************************
  * Tegra210 SiP SMCs
  ******************************************************************************/
 #define TEGRA_SIP_PMC_COMMANDS		U(0xC2FFFE00)
+#define TEGRA_SIP_EMC_COMMANDS		U(0xC2FFFE01)
 
 /*******************************************************************************
  * This function is responsible for handling all T210 SiP calls
@@ -45,6 +52,8 @@ int plat_sip_handler(uint32_t smc_fid,
 		     uint64_t flags)
 {
 	uint32_t val, ns;
+	uint64_t val2;
+	const plat_params_from_bl2_t *plat_params = bl31_get_plat_params();
 
 	/* Determine which security state this SMC originated from */
 	ns = is_caller_non_secure(flags);
@@ -84,6 +93,16 @@ int plat_sip_handler(uint32_t smc_fid,
 			write_ctx_reg(get_gpregs_ctx(handle), CTX_GPREG_X1, val);
 		} else if (x1 == PMC_WRITE) {
 			mmio_write_32((uint32_t)(TEGRA_PMC_BASE + x2), (uint32_t)x3);
+		} else {
+			return -EINVAL;
+		}
+	} else if (smc_fid == TEGRA_SIP_EMC_COMMANDS) {
+		if (x1 == EMC_TABLE_ADDR) {
+			val2 = plat_params->emc_table_base;
+			write_ctx_reg(get_gpregs_ctx(handle), CTX_GPREG_X1, val2);
+		} else if (x1 == EMC_TABLE_SIZE) {
+			val2 = plat_params->emc_table_size;
+			write_ctx_reg(get_gpregs_ctx(handle), CTX_GPREG_X1, val2);
 		} else {
 			return -EINVAL;
 		}
